1c73e479cc9d55177a6ae0f378b33edfd595ecde
stephen-eades <stephen.a.eades@gmail.com>
2023-04-12 10:51:08 -0400

    finished classes

--- basics/classes.cpp
+++ basics/classes.cpp
@@ -4,13 +4,22 @@
 #include <string>
 using namespace std;
 
-// Create a class
+// Create a class with attributes and a method
 class Car {
   public:
     string brand;   
     string model;
     int year;
+    void startCar() { 
+        cout << "The car is started";
+    }
+    void turnOffCar();
 };
+
+// Define a method outside of the class 
+void Car::turnOffCar() {
+  cout << "The car is turned off";
+}
 
 int main ()
 {
@@ -19,17 +28,20 @@ int main ()
     car1.brand = "BMW";
     car1.model = "X5";
     car1.year = 2019;
+    car1.startCar();
 
     // Create another object of Car
     Car car2;
     car2.brand = "Ford";
     car2.model = "Mustang";
     car2.year = 1999;
+    car2.startCar();
 
     Car car3;
     car3.brand = "Honda";
     car3.model = "Accord";
     car3.year = 2023;
+    car3.startCar();
 
     // Print out the values
     cout << car1.brand << endl << car2.brand << endl << car3.brand << endl;
